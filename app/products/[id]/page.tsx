f8004031f391519b8d102041998df078cf3d9de2
Pritom Dey <pritomdey4@gmail.com>
2025-05-19 13:02:20 +0600

    fixed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,26 +5,27 @@ import { books } from "@/helper/books";
 import { getRelatedBooks } from "@/helper/relatedBooks";
 import { Book } from "@/interfaces/interface";
 import React from "react";
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-export default async function page({ params }: PageProps) {
-  const { id } = params;
-  const bid = parseInt(id, 10);
+
+export default function Page({ params }: { params: { id: string } }) {
+  const bid = parseInt(params.id, 10);
   const book: Book | undefined = books.find((book) => book.id === bid);
+
+  if (!book) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-2xl text-red-600">Book not found.</p>
+      </div>
+    );
+  }
+
   const relatedBooks: Book[] = getRelatedBooks(book);
+
   return (
     <section className="min-h-screen pt-3 pb-10">
-      {/* book details and related books  */}
       <div className="w-9/10 md:w-8/10 bg-white mx-auto rounded-2xl shadow-xl mt-10 py-10 px-5 grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* book details */}
         <BookDetails book={book} />
-        {/* related books */}
-        <div className="w-full p-5 rounded-lg ">
+        <div className="w-full p-5 rounded-lg">
           <h1 className="font-bold text-xl md:text-2xl mb-5">Related Books</h1>
-
           <div className="grid gap-3">
             {relatedBooks.map((relatedBook, index) => (
               <RelatedBooks key={index} relatedBook={relatedBook} />
@@ -32,8 +33,6 @@ export default async function page({ params }: PageProps) {
           </div>
         </div>
       </div>
-
-      {/* books review and love */}
       <div className="w-9/10 md:w-8/10 bg-white mx-auto rounded-2xl shadow-xl mt-10 py-10 px-5">
         <Review />
       </div>
